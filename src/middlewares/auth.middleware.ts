20749f95747dbec47da359d17589ef1ffa8bd06a
Thomas Morgana <quentin@codebuds.com>
2023-08-28 17:46:27 +0200

    feat: add role usage in createAdmin route

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import userService from "../services/user.services";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { AuthenticatedRequest } from "../interfaces/auth.interface";
-import { User } from "@prisma/client";
 
 export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
   const token: string | undefined = req.header('Authorization')?.replace('Bearer ', '');
@@ -10,13 +9,8 @@ export const authenticateToken = async (req: Request, res: Response, next: NextF
   if (!token) return res.status(401).send("Token not found");
 
   try {
-    console.log(token);
-    
-    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
-
-    const user: User | null = await userService.retrieveById((req as AuthenticatedRequest).token.id);
-
-    console.log(decoded, user);
+    const decoded = jwt.verify(token, String(process.env.JWT_ACCESS_SECRET));
+    const user = await userService.retrieveById((decoded as JwtPayload).id);
     
     if(!user) {
       res.status(404).send("The user associated with this token does not exist anymore")
--- src/prisma/seeds/user.seeds.ts
+++ src/prisma/seeds/user.seeds.ts
@@ -1,10 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 import { faker } from "@faker-js/faker";
+import bcrypt from 'bcrypt';
+
 
 export const seedUsers = async (prisma: PrismaClient) => {
   console.info("User seeding started")
 
   await prisma.user.deleteMany({})
+
+  await prisma.user.create({
+    data: {
+      email: "admin@mail.com",
+      role: Role.ADMIN,
+      password: await bcrypt.hash("password", 12),
+    }
+  })
   
   const amountOfUsers = 10;
 
@@ -12,7 +22,7 @@ export const seedUsers = async (prisma: PrismaClient) => {
     await prisma.user.create({
       data: {
         email: faker.internet.email(),
-        password: "password",
+        password: await bcrypt.hash("password", 12),
       }
     })
   }
