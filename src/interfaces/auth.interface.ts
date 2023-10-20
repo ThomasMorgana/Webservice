049727cb8814206a3253804a24f3e90ac0fd6023
Thomas Morgana <thomas@codebuds.com>
2023-10-20 10:15:59 +0200

    cleanup

--- src/interfaces/auth.interface.ts
+++
@@ -1,8 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-import { Request } from "express";
-import { Role } from "@prisma/client";
-
-export interface AuthenticatedRequest extends Request {
-  token: JwtPayload
-  role: Role
-}
