b08c106cf5ed700b0f357558d8879341d01fb5a1
Gabriel Touron <gtcode@pm.me>
2023-11-27 00:00:36 +0100

    feat: add changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,9 @@
 import express, { Application } from 'express';
 import Server from './utils/server';
-import { logger } from './utils/logger';
+import { ContextService } from './utils/context.service';
 
 const app: Application = express();
-logger.info('express init');
-new Server(app);
-logger.info('server init');
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8082;
-logger.info(PORT);
+const contextService = ContextService.from();
 
-app
-  .listen(PORT, function () {
-    logger.info(`Server is running on port ${PORT}.`);
-  })
-  .on('error', logger.error);
+Server.createServer(app, contextService).start();
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,35 +3,48 @@ import { logRequest } from './logger';
 import Routes from '../routes';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from '../config';
+import { ContextService } from './context.service';
 
 export default class Server {
-  constructor(app: Application) {
-    this.configureExpress(app);
-    this.configureSwagger(app);
-    this.setupMiddleware(app);
-    this.initializeRoutes(app);
+  constructor(
+    readonly app: Application,
+    readonly contextService: ContextService,
+  ) {}
+
+  start(): void {
+    const { logger, port } = this.contextService;
+
+    this.app
+      .listen(port, () => {
+        logger.info(`Server is running on port ${port}.`);
+      })
+      .on('error', (error) => logger.error(error));
   }
 
-  private configureExpress(app: Application): void {
+  static createServer(app: Application, contextService: ContextService): Server {
+    contextService.logger.info('express started');
     app.use('/api/subscription/stripe-hook', express.raw({ type: '*/*' }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-  }
+    contextService.logger.info('express initialized');
 
-  private configureSwagger(app: Application): void {
+    contextService.logger.info('swagger initializing');
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.get('/', (_req, res) => {
       res.redirect('/api-docs');
     });
-  }
+    contextService.logger.info('swagger initialized');
 
-  private setupMiddleware(app: Application): void {
+    contextService.logger.info('middleware initializing');
     // DISABLED FOR GCP DEPLOYEMNT
     //app.use(rateLimit(LimiterConfig));
     app.use(logRequest);
-  }
+    contextService.logger.info('middleware initialized');
 
-  private initializeRoutes(app: Application): void {
+    contextService.logger.info('routes initializing');
     new Routes(app);
+    contextService.logger.info('routes initialized');
+
+    return new Server(app, contextService);
   }
 }
