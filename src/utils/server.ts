4acc1a87cc35353f29d02559c7bf202e95414571
Thomas Morgana <thomas@codebuds.com>
2023-12-04 12:04:17 +0100

    feat: refactor entrypoint to separate context from server start

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,35 +3,48 @@ import { logRequest } from './logger';
 import Routes from '../routes';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from '../config';
+import { AppContext } from './app_context';
 
 export default class Server {
-  constructor(app: Application) {
-    this.configureExpress(app);
-    this.configureSwagger(app);
-    this.setupMiddleware(app);
-    this.initializeRoutes(app);
+  constructor(
+    readonly app: Application,
+    readonly appContext: AppContext,
+  ) {}
+
+  start(): void {
+    const { logger, port } = this.appContext;
+
+    this.app
+      .listen(port, () => {
+        logger.info(`Server is running on port ${port}.`);
+      })
+      .on('error', (error) => logger.error(error));
   }
 
-  private configureExpress(app: Application): void {
+  static from(app: Application, { port, logger }: AppContext): Server {
+    logger.info('express started');
     app.use('/api/subscription/stripe-hook', express.raw({ type: '*/*' }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-  }
+    logger.info('express initialized');
 
-  private configureSwagger(app: Application): void {
+    logger.info('swagger initializing');
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.get('/', (_req, res) => {
       res.redirect('/api-docs');
     });
-  }
+    logger.info('swagger initialized');
 
-  private setupMiddleware(app: Application): void {
+    logger.info('middleware initializing');
     // DISABLED FOR GCP DEPLOYEMNT
     //app.use(rateLimit(LimiterConfig));
     app.use(logRequest);
-  }
+    logger.info('middleware initialized');
 
-  private initializeRoutes(app: Application): void {
+    logger.info('routes initializing');
     new Routes(app);
+    logger.info('routes initialized');
+
+    return new Server(app, { logger, port });
   }
 }
